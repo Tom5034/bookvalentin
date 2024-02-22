94b46de475967c1746d8b110c2cbd68647aa5e4a
Tom5034 <tom.samieri@gmail.com>
2024-02-22 14:22:26 +0100

    first commit

---
+++ scriptbook.js
@@ -0,0 +1,9 @@
+function openImg() {
+    var image = document.getElementById('image');
+    var source = image.src;
+    window.open(source)
+}
+
+
+
+//<script src="scriptbook.js"></script> (a mettre en desous de l'image a cliquer et mettre dans img le id="image" et onclick="openImg()")
